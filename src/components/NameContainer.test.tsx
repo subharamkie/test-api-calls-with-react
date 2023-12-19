5e6469fbf81e14f3afb611d17f1063c9b3c1abbb
Subha <subharamkie@gmail.com>
2023-12-19 09:21:36 +0000

    Add:Added alternative way to test for component to be loaded

--- src/components/NameContainer.test.tsx
+++ src/components/NameContainer.test.tsx
@@ -2,10 +2,14 @@ import {render,screen,waitFor} from '@testing-library/react';
 import { NameContainer } from './NameContainer';
 test('testing if name container loads',async ()=>{
     render(<NameContainer/>)
+    const nameCon = await screen.findByText('First',{exact:false});
+    expect(nameCon).toBeInTheDocument();
+    /*
     await waitFor(()=>{
        //expect(screen.queryByText('Loading...')).toBeNull();
        const nameCon = screen.getByText("First");
     expect(nameCon).toBeInTheDocument();
     });
+    */
     
 })
